7f064acfc4bc9590b828dc8f2b52eecfe02cd497
Alex Roussiere <alex.roussiere@snapsheet.me>
2019-04-06 01:03:34 -0500

    code cleaned. Just missing more questions in json

--- lib/MyQuestionWidget.dart
+++ lib/MyQuestionWidget.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class MyQuestionWidget extends StatelessWidget {
-  MyQuestionWidget({Key key, this.question, this.controller})
-      : questionAnimation = Tween<double>(begin: 1, end: 0).animate(controller);
+  MyQuestionWidget({this.question, this.controller})
+      : opacityAnimation = Tween<double>(begin: 1, end: 0).animate(controller);
 
   final String question;
   final Animation<double> controller;
-  final Animation<double> questionAnimation;
+  final Animation<double> opacityAnimation;
 
   Widget _buildAnimation(BuildContext context, Widget child) {
     return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Center(
           child: Opacity(
-            opacity: questionAnimation.value,
+            opacity: opacityAnimation.value,
             child: Text(
               question,
               textScaleFactor: 2.5,
