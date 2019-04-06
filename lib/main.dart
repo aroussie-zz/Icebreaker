ca026aa16ed7c98c2f3b166101d481137725cebd
Alex Roussiere <alex.roussiere@snapsheet.me>
2019-04-05 23:27:34 -0500

    code cleaned and commented

--- lib/main.dart
+++ lib/main.dart
@@ -22,94 +22,105 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   List<String> _icebreakers = ["Welcome message"];
 
-  AnimationController _animationController;
-  AnimationController _questionAnimationController;
+  AnimationController _colorAnimationController;
+  AnimationController _textAnimationController;
   CurvedAnimation _curvedAnimation;
   Animation<Color> _colorAnimation;
   Animation<double> _questionAnimation;
   Tween<Color> _colorTween;
   int _currentColorIndex = 0;
   int _currentQuestionIndex = 0;
-  String myQuestion;
+  String _currentQuestion;
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
+    _colorAnimationController = AnimationController(
       vsync: this,
       duration: Duration(milliseconds: 500),
     );
 
-    _questionAnimationController = AnimationController(
+    _textAnimationController = AnimationController(
       vsync: this,
       duration: Duration(milliseconds: 250),
     );
 
-    _curvedAnimation =
-        CurvedAnimation(parent: _animationController, curve: Curves.easeOut);
+    _curvedAnimation = CurvedAnimation(
+        parent: _colorAnimationController, curve: Curves.easeOut);
 
     _colorTween = ColorTween(begin: Colors.orange, end: Colors.orange);
 
     _colorAnimation = _colorTween.animate(_curvedAnimation);
+
     _questionAnimation =
-        Tween<double>(begin: 1, end: 0).animate(_questionAnimationController);
+        Tween<double>(begin: 1, end: 0).animate(_textAnimationController);
 
     rootBundle.loadString("assets/questions.json").then((data) {
-      _icebreakers.addAll((json.decode(data) as List).map(((element) => element.toString())).toList());
+      _icebreakers.addAll((json.decode(data) as List)
+          .map(((element) => element.toString()))
+          .toList());
     });
 
-    myQuestion = _icebreakers[0];
+    _currentQuestion = _icebreakers[0];
+  }
 
+  @override
+  void dispose() {
+    super.dispose();
+    _colorAnimationController.dispose();
+    _textAnimationController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: _colorAnimation.value,
         body: GestureDetector(
-            onTap: animateToNewColor,
-            child: Stack(fit: StackFit.expand, children: <Widget>[
-              AnimatedBuilder(
-                  animation: _colorAnimation,
-                  builder: (_, child) {
-                    return Scaffold(
-                        backgroundColor: _colorAnimation.value,
-                        body: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
-                            child: Align(
-                              child: Opacity(
-                                opacity: _questionAnimation.value,
-                                child: Text(
-                                  myQuestion,
-                                  textScaleFactor: 2.5,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            )));
-                  }),
-            ])));
+          onTap: _animateColorAndText,
+          child: AnimatedBuilder(
+              animation: _colorAnimation,
+              builder: (_, child) {
+                return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Center(
+                      child: Opacity(
+                        opacity: _questionAnimation.value,
+                        child: Text(
+                          _currentQuestion,
+                          textScaleFactor: 2.5,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ));
+              }),
+        ));
   }
 
-  void animateToNewColor() {
+  void _animateColorAndText() {
+    //Switch to a different color
     _colorTween.begin = _colorTween.end;
-    _animationController.reset();
-    _colorTween.end = generateRandomColor();
-    _animationController.forward();
-    _questionAnimationController.forward().whenComplete(() {
+    _colorAnimationController.reset();
+    _colorTween.end = _generateRandomColor();
+    _colorAnimationController.forward();
+
+    // Fade out the text to change it behind the scene and fade it back in
+    _textAnimationController.forward().whenComplete(() {
       //If reached the end of the questions, we start from the beginning again
       _currentQuestionIndex = _currentQuestionIndex == _icebreakers.length - 1
-          ? 0 : _currentQuestionIndex +=1;
-      myQuestion = _icebreakers[_currentQuestionIndex];
-      _questionAnimationController.reverse();
+          ? 0
+          : _currentQuestionIndex += 1;
+      _currentQuestion = _icebreakers[_currentQuestionIndex];
+      _textAnimationController.reverse();
     });
   }
 
-  Color generateRandomColor() {
+  Color _generateRandomColor() {
     var availableColors = Colors.primaries.sublist(0, 9);
-    int a = Random().nextInt(availableColors.length);
-    if (a != _currentColorIndex) {
-      _currentColorIndex = a;
-      return availableColors[a];
+    int randomIndex = Random().nextInt(availableColors.length);
+    if (randomIndex != _currentColorIndex) {
+      _currentColorIndex = randomIndex;
+      return availableColors[randomIndex];
     } else
-      return generateRandomColor();
+      return _generateRandomColor();
   }
 }
